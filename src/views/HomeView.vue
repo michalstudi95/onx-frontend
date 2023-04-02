d93c84a581b1dae57b25584701e71da84a3d48e5
Michał Stodolny <mich.studiukb@gmail.com>
2023-04-02 19:30:27 +0200

    The end.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,24 @@
-<template>Home</template>
+<template>
+  <p>
+    Dzień dobry, nazywam się Michał Stodolny i chcę podziękować za możliowść
+    udziału w rekrutacji w Państwa firmie.
+  </p>
+  <p>
+    Niestety nie udało mi się wykonać wszystkich zadań. Oto lista czego nie
+    zrobiłem:
+  </p>
+
+  <h5>Laravel</h5>
+  <ul>
+    <li>przypisywanie pracownika do auta</li>
+    <li>zadanie numer 3, test HTTP</li>
+    <li>zadanie numer 4, system notyfikacji</li>
+  </ul>
+
+  <h5>Vue</h5>
+  <ul>
+    <li>wyszkuiwanie klientów</li>
+    <li>autentykacja</li>
+    <li>zadanie numer 5</li>
+  </ul>
+</template>
